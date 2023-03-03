9e1995387228f6e211e48705ec76423001e2b778
code-xiake <119009262+code-xiake@users.noreply.github.com>
2023-03-03 20:32:11 +0100

    Add files via upload
    this is the initial smartcontract uploaded on the goerli testnet

---
+++ EthereumSavingBox4.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.5/contracts/security/ReentrancyGuard.sol";
+
+contract SavingBox is ReentrancyGuard {
+    address owner;
+    uint256 withdrawalLimit;
+    
+    constructor() {
+        owner = msg.sender;
+        withdrawalLimit = 0;
+    }
+    
+    receive() external payable {}
+    
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    
+    function setWithdrawalLimit(uint256 _limit) public {
+        require(msg.sender == owner, "Only the owner can set the withdrawal limit.");
+        withdrawalLimit = _limit;
+    }
+    
+    function withdraw(uint256 _amount) public nonReentrant payable {
+        require(msg.sender == owner, "Only the owner can withdraw.");
+        require(getBalance() >= _amount && (_amount <= withdrawalLimit || withdrawalLimit == 0), "Insufficient balance to withdraw or withdrawal limit exceeded.");
+        payable(owner).transfer(_amount);
+    }
+}
